1a8a789922801917ce365b4b02d80acae3640762
SANTEOWEN <owensante45@gmail.com>
2024-02-11 17:19:42 +0800

    Done Dart loops

--- learning_dart/4-[LOOPS]/forLoop.dart
+++ learning_dart/4-[LOOPS]/forLoop.dart
@@ -1,10 +1,17 @@
 void main() {
-  for (int i = 1; i <= 10; i++) {
+  for (int i = 1; i <= 1; i++) {
     String hi = 'Hello world!';
     print(hi.substring(0, 5));
     print('LAMAW count: $i');
+  }
+
+  String greet = 'Hello';
+  for (int i = 0; i < greet.length; i++) {
+    print(greet[i]);
   }
 }
 
 //for loop is basically the same with other languages a but still dont forget about 
 //Type strict of variables as much as we can
+
+//also dont forget the Zero Index counting in integers
---
+++ learning_dart/4-[LOOPS]/whileLoop.dart
@@ -0,0 +1,10 @@
+void main() {
+  int i = 0;
+
+  String value = 'Hello';
+
+  while (i < value.length) {
+    print(value[i]);
+    i++;
+  }
+}
