b3a055fb61e5cee06049a2d4d4f68161ab8145a0
SANTEOWEN <owensante45@gmail.com>
2024-02-11 17:01:03 +0800

    LEARNED FOR LOOP Under dart

---
+++ learning_dart/4-[LOOPS]/forLoop.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    String hi = 'Hello world!';
+    print(hi.substring(0, 5));
+    print('LAMAW count: $i');
+  }
+}
+
+//for loop is basically the same with other languages a but still dont forget about 
+//Type strict of variables as much as we can
